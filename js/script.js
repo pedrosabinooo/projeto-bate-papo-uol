f2bac67ad5bdf2d4b37a94332a7d5fb961c1e75f
pedrosabinooo <pedrosabino@outlook.com>
2022-02-11 00:47:07 -0300

    Todos os requests montados.

--- js/script.js
+++ js/script.js
@@ -8,12 +8,20 @@ sidebar.classList.add("hidden");
 let userName;
 let mainChat;
 
-// Chamando as funções -------------------------------------------
-const input = document.querySelector(".login input");
-input.addEventListener("keyup", function (event) {
+// Chamando as funções para enviar texto com Enter ---------------
+const inputLogin = document.querySelector(".login input");
+inputLogin.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
         document.querySelector(".login button").click();
+    }
+});
+
+const inputMessage = document.querySelector("footer input");
+inputMessage.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.querySelector("footer button").click();
     }
 });
 
@@ -34,11 +42,12 @@ function login() {
 }
 
 function successfulLogin() {
-    console.log("Login com sucesso!");
+    // console.log("Login com sucesso!");
     const divLogin = document.querySelector(".login");
     divLogin.classList.add("hidden");
     setInterval(checkUserStatus, 5000);
     setInterval(loadMessages, 3000);
+    setInterval(loadOnlineContacts, 10000);
 }
 
 function failedLogin(error) {
@@ -60,14 +69,16 @@ function checkUserStatus() {
 }
 
 function stillInTheRoom() {
-    console.log("Ainda na sala!");
+    // console.log("Ainda na sala!");
 }
 
 function outTheRoom(error) {
-    console.log("Saiu da sala!");
+    // console.log("Saiu da sala!");
+    alert("Você saiu. Entre novamente com o seu nome.");
+    window.location.reload();
 }
 
-// Função para recarregar as mensagens a cada 3 segundos
+// Função para recarregar as mensagens
 function loadMessages() {
     let promise = axios.get(
         "https://mock-api.driven.com.br/api/v4/uol/messages"
@@ -77,35 +88,38 @@ function loadMessages() {
 }
 
 function loadingMessages(response) {
-    console.log("Mensagens carregadas :)");
+    // console.log("Mensagens carregadas :)");
     // console.log(response.data);
     displayMessages(response.data);
 }
 
 function errorLoadingMessages(error) {
-    console.log("Não carregou as mensagens :(");
+    console.log("Não carregou as mensagens. Erro: " + error.response.status);
 }
 
 // Função para carregar as mensagens no chat
 function displayMessages(messages) {
-    console.log("entrei na displayMessages")
     mainChat = document.querySelector("main");
-    console.log("carreguei o mainChat")
-    for (let i=0; i < messages.lenght; i++) {
-        console.log(messages[i]);
-        if (messages[i].type == 'status') {
-            statusMessage(messages[i], mainChat);
-        } else if (messages[i].type === 'message') {
-            textMessage(messages[i], mainChat);
-        } else if (messages[i].type === 'private_message' && (messages[i].from === userName || messages[i].to === userName)) {
-            privateMessage(messages[i], mainChat);
-        }
-    } // PAREI AQUI !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
+    // console.log(messages)
+    for (let i=0; i < messages.length; i++) {
+        // console.log(messages[i]);
+        // if (i==0 || messages[i].time > mainChat.querySelector(".message:last-child").value){
+            if (messages[i].type == 'status') {
+                statusMessage(messages[i], mainChat);
+            } else if (messages[i].type === 'message') {
+                textMessage(messages[i], mainChat);
+            } else if (messages[i].type === 'private_message' && (messages[i].from === userName || messages[i].to === userName)) {
+                privateMessage(messages[i], mainChat);
+            }
+            let message = document.querySelector("main .message:last-child");
+            message.scrollIntoView();
+        // }
+    } // LEMBRAR DE TIRAR ESSES COMENTÁRIOS -------------------------------------------------------------------
 }
 
 function statusMessage(message) {
     mainChat.innerHTML += `
-        <div class="in-out">
+        <div class="in-out message">
             <span class="time">(${message.time})</span>
             <span class="from-to"><span class="username">${message.from}</span></span>
             <span class="text" data-identifier="message">${message.text}</span>
@@ -115,7 +129,7 @@ function statusMessage(message) {
 
 function textMessage(message) {
     mainChat.innerHTML += `
-        <div class="message">
+        <div class="public message">
             <span class="time">(${message.time})</span>
             <span class="from-to"><span class="username">${message.from}</span> para <span class="username">${message.to}</span>:</span>
             <span class="text" data-identifier="message">${message.text}</span>
@@ -133,6 +147,83 @@ function privateMessage(message) {
     `
 }
 
+// Funções para enviar as mensagens
+function sendMessage() {
+    const toUser = document.querySelector("aside .contacts .selected").innerHTML;
+    const message = document.querySelector("footer input").value;
+    const visibility = document.querySelector("aside .visibility .selected").innerHTML;
+    
+    if (visibility==='Público') {
+        var type = "message"
+    } else {
+        var type = "private_message"
+    }
+
+    let promise = axios.post(
+        "https://mock-api.driven.com.br/api/v4/uol/messages",
+        {
+            from: userName,
+            to: toUser,
+            text: message,
+            type: type
+        }
+    );
+    promise.then(messageSent);
+    promise.catch(messageNotSent);
+    document.querySelector("footer input").value = "";
+}
+
+function messageSent(response) {
+    // console.log("Enviou a mensagem!");
+    // console.log(response);
+}
+
+function messageNotSent(error) {
+    console.log("Erro no envio! Erro: " + error.response.status);
+}
+
+// Função para carregar os participantes online
+function loadOnlineContacts() {
+    let promise = axios.get(
+        "https://mock-api.driven.com.br/api/v4/uol/participants"
+    );
+    promise.then(loadingContacts);
+    promise.catch(errorLoadingContacts);
+}
+
+function loadingContacts(response) {
+    // console.log("Contatos carregados :)");
+    console.log(response.data);
+    displayContacts(response.data);
+}
+
+function errorLoadingContacts(error) {
+    console.log("Não carregou os contatos. Erro: " + error.response.status);
+}
+
+function displayContacts(contacts) {
+    let divContacts = document.querySelector("aside .contacts");
+    divContacts.innerHTML += `
+        <div class="contact all">
+            <figure><img src="media/people.svg" alt="Todos"></figure>
+            <div class="checked-style">
+                <span class="selected">Todos</span>
+                <img src="media/check.svg" alt="Selected">
+            </div>
+        </div>
+    `
+    for (let i=0;i<contacts.length;i++) {
+        divContacts.innerHTML += `
+            <div class="contact person" data-identifier="participant">
+                <figure><img src="media/person.svg" alt="${contacts[i].name}"></figure>
+                <div class="checked-style">
+                    <span>${contacts[i].name}</span>
+                    <img class="hidden" src="media/check.svg" alt="Selected">
+                </div>
+            </div>
+        `
+    }
+}
 
 // Funções para navegar no chat
 function closeSidebar(button) {
